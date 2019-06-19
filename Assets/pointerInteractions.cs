ec9fb9f9f88d2b8c50801b454aac18d1e448895d
Angelina <h1255513@gmail.com>
2019-06-19 15:44:27 -0400

    can add and delete objects

---
+++ Assets/pointerInteractions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class pointerInteractions : MonoBehaviour
+{
+    VRTK_Pointer pointer;
+    public GameObject person;
+    public GameObject projector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointer = GetComponent<VRTK_Pointer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pointer.pointerRenderer != null)
+        {
+            //Add person
+            if (pointer.pointerRenderer.GetDestinationHit().collider.gameObject.name == "floor")
+            {
+                if (OVRInput.GetDown(OVRInput.Button.Three))
+                {
+                    Instantiate(person, new Vector3(pointer.pointerRenderer.GetDestinationHit().point.x, 0.5f, pointer.pointerRenderer.GetDestinationHit().point.z), Quaternion.identity);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            //Add Projector
+
+            else if (pointer.pointerRenderer.GetDestinationHit().collider.gameObject.name == "Ceiling")
+            {
+                if (OVRInput.GetDown(OVRInput.Button.Four))
+                {
+                    Instantiate(projector, new Vector3(pointer.pointerRenderer.GetDestinationHit().point.x, 4.38f, pointer.pointerRenderer.GetDestinationHit().point.z), Quaternion.identity);
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            //Delete Person && Projector
+            else if (pointer.pointerRenderer.GetDestinationHit().collider.tag == "person"||pointer.pointerRenderer.GetDestinationHit().collider.tag=="projector") {
+                if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger)==1)
+                {
+                    Destroy(pointer.pointerRenderer.GetDestinationHit().transform.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# Buttons:
+
+***Right Controller***
+
+Trigger: Teleport to pointer location
+
+Joystick (press): Toggle movable wall
+
+Grip: Grab/Ungrab an object ("person" cylinder/projector)
+
+Joystick Axis (Left/Right): (when pointing at projector) Rotate projector (Yaw)
+
+(Maybe): Joystick Axis (Up/Down): (when pointing at ceiling) Adjust ceiling height (has a maximum height at default, can only be lowered)
+
+A: (When pointing at a "person") Switch person (Adult, teen, child)
+
+
+
+<br />
+
+***Left Controller*** (not yet implemented):
+
+Grip: Activate pointer
+
+Trigger: Delete pointer Gameobject
+
+X: Instantiate new person at pointer location 
+
+Y: Instantiate new projector at pointer location
+
